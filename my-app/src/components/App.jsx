461283b15b2103c14777d83362d82dcf48616f8f
Thebrefff <filip_zielinski@o2.pl>
2023-05-14 16:44:09 +0200

    statistics - done

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -1,10 +1,10 @@
 import user from '../data/user.json';
-// import data from 'data/data.json';
+import data from '../data/data.json';
 // import friends from 'data/friends.json';
 // import transactions from 'data/transactions.json';
 
 import { Profile } from './profile/Profile';
-// import { Statistics } from 'components/statistics/Statistics';
+import { Statistics } from './statistics/Statistics';
 // import { FriendList } from 'components/friendList/FriendList';
 // import { TransactionHistory } from 'components/transactionHistory/TransactionHistory';
 
@@ -30,7 +30,7 @@ export const App = () => {
         stats={user.stats}
       />
 
-      {/* <Statistics title="Upload stats" stats={data} /> */}
+      <Statistics title="Upload stats" stats={data} />
 
       {/* <FriendList friends={friends} /> */}
 
