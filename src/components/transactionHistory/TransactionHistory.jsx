15b2d2012d54a88a8200c6a238f4ad7eb616e45b
Thebrefff <filip_zielinski@o2.pl>
2023-05-14 21:42:50 +0200

    finished

--- src/components/transactionHistory/TransactionHistory.jsx
+++ src/components/transactionHistory/TransactionHistory.jsx
@@ -4,21 +4,21 @@ import css from './TransactionHistory.module.css';
 
 export const TransactionHistory = ({ items }) => {
   return (
-    <table className={css.transHistory}>
-      <thead>
+    <table className={css.TransactionHistory}>
+      <thead className={css.tableHead}>
         <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+          <th className={css.tType}>Type</th>
+          <th className={css.tAmount}>Amount</th>
+          <th className={css.tCurrency}>Currency</th>
         </tr>
       </thead>
 
       <tbody>
         {items.map(item => (
-          <tr className={css.row} key={item.id}>
-            <td className={css.transData}>{item.type}</td>
-            <td className={css.transData}>{item.amount}</td>
-            <td className={css.transData}>{item.currency}</td>
+          <tr className={css.tableRow} key={item.id}>
+            <td className={css.tableColumn}>{item.type}</td>
+            <td className={css.tableColumn}>{item.amount}</td>
+            <td className={css.tableColumn}>{item.currency}</td>
           </tr>
         ))}
       </tbody>
