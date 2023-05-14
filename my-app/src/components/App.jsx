5b04c9b132a2da568d4162e537583d2fd97b8588
Thebrefff <filip_zielinski@o2.pl>
2023-05-14 16:53:58 +0200

    transactions - done

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -1,12 +1,12 @@
 import user from '../data/user.json';
 import data from '../data/data.json';
 // import friends from 'data/friends.json';
-// import transactions from 'data/transactions.json';
+import transactions from '../data/transactions.json';
 
 import { Profile } from './profile/Profile';
 import { Statistics } from './statistics/Statistics';
 // import { FriendList } from 'components/friendList/FriendList';
-// import { TransactionHistory } from 'components/transactionHistory/TransactionHistory';
+import { TransactionHistory } from './transactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -34,7 +34,7 @@ export const App = () => {
 
       {/* <FriendList friends={friends} /> */}
 
-      {/* <TransactionHistory items={transactions} /> */}
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
